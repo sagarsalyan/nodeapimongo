535d31c365a4af85cd66d0e6ac43c3e7febc86da
sagarsalyan <sagarslyn@gmail.com>
2020-04-13 20:10:01 +0530

    Added CORS

--- READMe.txt
+++ READMe.txt
@@ -8,4 +8,7 @@ ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
 flush privileges;
 
 Debug
-node --inspect --debug index.js
+> node --inspect --debug index.js
+
+Install CORS
+> npm install cors
--- server.js
+++ server.js
@@ -3,7 +3,16 @@ var express = require('express'),
   port = process.env.PORT || 3001,
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'), //created model loading here
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  cors = require('cors');
+
+
+var corsOption = {
+    origin:true,
+    methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials:true,
+    exposedHeaders:['x-auth-token']
+  };
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
@@ -12,6 +21,7 @@ mongoose.connect('mongodb://localhost/Tododb');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors(corsOption));
 
 
 var routes = require('./api/routes/todoListRoute'); //importing route
@@ -24,5 +34,8 @@ app.listen(port);
 console.log('todo list RESTful API server started on: ' + port);
 
 app.use(function(req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Headers', 'content-type');
+res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
